a2cfdf450936f7b5e9c91ed51918b5d5a745082e
HenryMarcos <thangtranmanh9@gmail.com>
2026-03-27 00:12:29 +0700

    Update base code and application creation

--- oas/src/main/java/module-info.java
+++ oas/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.groupproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
-    opens com.groupproject to javafx.fxml;
+    opens com.groupproject to javafx.fxml, javafx.graphics;
     exports com.groupproject;
 }
