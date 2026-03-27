fc99f9416f30af38a4e6e2eef3d3ff8f96f7ae48
HenryMarcos <thangtranmanh9@gmail.com>
2026-03-27 13:55:18 +0700

    Khởi tạo các lớp cơ bản

---
+++ oas/src/main/java/com/groupproject/base/item/Art.java
@@ -0,0 +1,7 @@
+// Base class for all art related item
+
+package com.groupproject.base.item;
+
+public class Art extends Item {
+    
+}
---
+++ oas/src/main/java/com/groupproject/base/user/Admin.java
@@ -0,0 +1,7 @@
+// The one who have all permission to edit and change thing around
+
+package com.groupproject.base.user;
+
+public class Admin extends User {
+    
+}
