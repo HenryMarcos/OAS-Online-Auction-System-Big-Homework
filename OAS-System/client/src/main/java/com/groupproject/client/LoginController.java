cf28d0b0278e0ebfac2df7237791e1f831cf9abc
tuanpro-lab <minhtuanhacker123lh@gmail.com>
2026-04-10 15:59:43 +0700

    tách file login.java

--- OAS-System/client/src/main/java/com/groupproject/client/LoginController.java
+++ OAS-System/client/src/main/java/com/groupproject/client/LoginController.java
@@ -1,20 +1,40 @@
 package com.groupproject.client;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 
 public class LoginController {
     @FXML
-    private Label login;
-    @FXML
-    private Label signuptext;
+    private TextField usernameField;
 
     @FXML
-    protected void onHelloButtonClick() {
-        login.setText("Hello");
-    }
+    private PasswordField passwordField;
+
     @FXML
-    protected void onSignupButtonClick() {
-        signuptext.setText("Sign up");
+    private TextField passwordTextField;
+
+    @FXML
+    private ToggleButton eyeButton;
+
+    @FXML
+    public void initialize() {
+        // Vẫn giữ dòng này để đồng bộ chữ giữa 2 ô nhập mật khẩu
+        passwordField.textProperty().bindBidirectional(passwordTextField.textProperty());
+    }
+
+    // Hàm này được gọi khi bấm nút con mắt (Do đã set onAction="#togglePasswordVisibility" trong FXML)
+    @FXML
+    private void togglePasswordVisibility(ActionEvent event) {
+         boolean isVisible = eyeButton.isSelected();
+        // Bật/tắt trạng thái hiển thị
+        passwordField.setManaged(!isVisible);
+        passwordField.setVisible(!isVisible);
+
+        passwordTextField.setManaged(isVisible);
+        passwordTextField.setVisible(isVisible);
+        
     }
 }
