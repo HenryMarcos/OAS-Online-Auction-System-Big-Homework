a194f7528883bdf10b5e850a3fc86bc13357c5d7
abeoabeo <truongpro2072007@gmail.com>
2026-04-11 19:40:43 +0700

    feat: chỉnh sửa lại các class

---
+++ OAS-System/shared/src/main/java/com/groupproject/shared/Bidder.java
@@ -0,0 +1,7 @@
+package com.groupproject.shared;
+
+public class Bidder extends User {
+    public Bidder(String username, String email, String password) {
+        super(username, email, password);
+    }
+}
